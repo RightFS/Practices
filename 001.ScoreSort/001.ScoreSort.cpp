031096a89e03eeb63b2325c58a250982510ce347
孙兆政 <zheng6686@live.com>
2019-02-19 00:00:31 +0800

    init

---
+++ 001.ScoreSort/001.ScoreSort.cpp
@@ -0,0 +1,55 @@
+﻿// ScoreSort.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//考稳定排序和升降序
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+typedef struct _student {
+	char name[10];
+	unsigned int score;
+	bool operator>(const _student s) {
+		return this->score > s.score;
+	}
+	bool operator<(const _student s) {
+		return this->score < s.score;
+	}
+}Student;
+template<typename T>
+void bubbleSort(T arr[], int len, int sortby) {
+	int i, j;  T temp;
+	if (sortby != 0) {
+		for (i = 0; i < len - 1; i++)
+			for (j = 0; j < len - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+	} else {
+		for (i = 0; i < len - 1; i++)
+			for (j = 0; j < len - 1 - i; j++) {
+				if (arr[j] < arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+	}
+
+}
+int main(int c, char** v) {
+	int count;
+	int sortby;
+	while (cin >> count >> sortby) {
+		Student* students = new Student[count];
+		for (int i = 0; i < count; i++) {
+			cin >> students[i].name >> students[i].score;
+		}
+		bubbleSort(students, count, sortby);
+		for (int i = 0; i < count; i++) {
+			cout << students[i].name << " " << students[i].score << endl;
+		}
+		delete[] students;
+	}
+}
---
+++ 002.FactorsCounter/002.FactorsCounter.cpp
@@ -0,0 +1,37 @@
+﻿// 002.FactorsCounter.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//题目描述
+//输入n个整数, 依次输出每个数的约数的个数
+//输入描述 :
+//输入的第一行为N，即数组的个数(N <= 1000)
+//接下来的1行包括N个整数，其中每个数的范围为(1 <= Num <= 1000000000)
+//当N = 0时输入结束。
+//输出描述 :
+//可能有多组输入数据，对于每组输入数据，
+//输出N行，其中每一行对应上面的一个数的约数的个数。
+
+//Tips:本题关键在于循环退出条件
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	while (cin >> n) {
+		int a;
+		int num = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> a;
+			int j;
+			for (j = 1; j*j < a; j++) {
+				if (a % j == 0) {
+					num += 2;
+				}
+			}
+			if (j*j == a) {
+				num++;
+			}
+			cout << num << endl;
+			num = 0;
+		}
+	}
+}
